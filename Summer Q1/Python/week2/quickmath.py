095f94bbd91df65b9cd4cc016c0fc523c05fec03
Carl Sullivan <Carl.Sullivan@hgs.hiddengeniusproject.org>
2025-06-24 14:00:04 -0700

    quickmathpy

---
+++ Summer Q1/Python/week2/quickmath.py
@@ -0,0 +1,8 @@
+#1. declare and assign 2 variables 
+num1 = -77
+num2 = 7
+
+#2. declare ans assign a 3rd variable that is the sum of the first
+num3 = num1 + num2
+#3. print the 3rd variable to the terminal
+print("The Sum is" + num3)
