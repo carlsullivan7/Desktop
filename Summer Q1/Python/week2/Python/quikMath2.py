02e91603fbeca48c6d68dacd44b675d36199b2a7
Carl Sullivan <Carl.Sullivan@hgs.hiddengeniusproject.org>
2025-06-30 13:40:35 -0700

    loopsexit

---
+++ Summer Q1/Python/week2/Python/quikMath2.py
@@ -0,0 +1,31 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+#Call our add funtion and store its output inside the sums variable
+sum = Add(5, 35)
+print(sum)
+#print the sum variable
+
+def Add(addend3, addend4):
+    return addend3 + addend4
+
+sum = Add(10, 78)
+print(sum)
+
+def Sub (subend1, subend2):
+    return subend1 - subend2
+
+sum = Sub (67,45)
+print(sum)
+
+def Div (div1, div2):
+    return div1 / div2
+
+sum = div(10,3)
+print(sum)
+
+def mul (mul1, mul2):
+    return mul1 * mul2
+
+sum = mul(6,7)
+print(sum)
